21c97a41ffd9b05137a3b4dd46964c96c76ad1e7
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2022-11-17 10:10:45 +0000

    cargar icono

---
+++ app/src/main/java/com/example/aut2_02proyectoandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.aut2_02proyectoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+}
