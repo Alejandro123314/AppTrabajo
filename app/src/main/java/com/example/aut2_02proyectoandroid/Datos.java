66ab984e52d7287eb9d4a52020b846ae34a6b50a
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2022-11-23 10:13:20 +0000

    visualizar imagenes

---
+++ app/src/main/java/com/example/aut2_02proyectoandroid/Datos.java
@@ -0,0 +1,23 @@
+package com.example.aut2_02proyectoandroid;
+
+public class Datos {
+    private String texto1;
+    private String texto2;
+    private String texto3;
+
+    public Datos (String text1, String text2, String text3){
+        texto1 = text1;
+        texto2 = text2;
+        texto3 = text3;
+    }
+
+    public String getTexto1() {
+        return texto1;
+    }
+    public String getTexto2() {
+        return texto2;
+    }
+    public String getTexto3() {
+        return texto3;
+    }
+}
--- app/src/main/java/com/example/aut2_02proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/aut2_02proyectoandroid/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.aut2_02proyectoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Datos[] datos = new Datos[]{
+                new Datos("Cabaña en la esquina", "Linea Medio 1", "12.323€"),
+                new Datos("Casa del Molino", "Linea Medio 2", "14.678€"),
+                new Datos("Los aguilones", "Linea Medio 3", "18.592€"),
+                new Datos("Casa Roja", "Linea Medio 4", "8.397€")};
 
+        ListView listadoMarcas = (ListView) findViewById(R.id.ListAdapater);
+        View cabeceraGV = getLayoutInflater().inflate(R.layout.cabezada, null);
+        listadoMarcas.addHeaderView(cabeceraGV);
+        Adaptador adaptador = new Adaptador(this, datos);
 
+        listadoMarcas.setAdapter(adaptador);
+
+        listadoMarcas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView marcaSeleccionada = (TextView) findViewById(R.id.tvMensajeMarca1);
+                String marca = ((Datos) adapterView.getItemAtPosition(i)).getTexto1();
+                marcaSeleccionada.setText(marca);
+
+                TextView descripcionSeleccionada = (TextView) findViewById(R.id.tvMensajeDescripcion);
+                String descripcion = ((Datos)adapterView.getItemAtPosition(i)).getTexto2();
+                descripcionSeleccionada.setText(descripcion);
+            }
+        });
     }
 }
